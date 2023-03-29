1c71f4b9beed1fc4faaee7c41f16dd5a807b5dc4
Krzysztof Czereczon <kczereczon@gmail.com>
2023-03-29 20:25:35 +0200

    created api

--- src/Entity/Transaction.php
+++ src/Entity/Transaction.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TransactionRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ApiResource]
 class Transaction
 {
     #[ORM\Id]
