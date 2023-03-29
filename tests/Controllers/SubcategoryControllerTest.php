d1096625ef590242db3033844e132eea44876148
Krzysztof Czereczon <kczereczon@gmail.com>
2023-03-29 20:19:05 +0200

    added subcategory

--- tests/Controllers/SubcategoryControllerTest.php
+++ tests/Controllers/SubcategoryControllerTest.php
@@ -3,15 +3,12 @@
 namespace App\Tests\Controllers;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use App\DataFixtures\CategoryFixtures;
 use App\Entity\Category;
-use App\Tests\Traits\RefreshDatabase;
+use App\Entity\SubCategory;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
 use Faker\Factory;
-use Faker\Generator;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
@@ -19,10 +16,8 @@ use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
-class CategoryTest extends ApiTestCase
+class SubcategoryControllerTest extends ApiTestCase
 {
-    private ?EntityManager $entityManager;
-    private Generator $faker;
 
     /**
      * @throws \Exception
@@ -44,25 +39,33 @@ class CategoryTest extends ApiTestCase
      * @throws ClientExceptionInterface
      * @throws ORMException
      */
-    public function testPaginationStructure(): void
+    public function testPagination(): void
     {
-        foreach (range(1, 100) as $i) {
+        foreach (range(1, 25) as $i) {
             $category = new Category();
             $category->setName($this->faker->domainName());
             $category->setColor($this->faker->colorName());
             $this->entityManager->persist($category);
+
+            foreach (range(1, 4) as $j) {
+                $subcategory = new SubCategory();
+                $subcategory->setName($this->faker->domainName());
+                $subcategory->setCategory($category);
+                $this->entityManager->persist($subcategory);
+            }
         }
+
 
         $this->entityManager->flush();
 
-        static::createClient()->request('GET', '/category');
+        $response = static::createClient()->request('GET', '/subcategory');
 
         self::assertResponseIsSuccessful();
         self::assertJsonContains([
             'pages' => 4,
             'page' => 1,
-            'next' => '/category?page=2&limit=25',
-            'previous' => '/category?page=1&limit=25',
+            'next' => '/subcategory?page=2&limit=25',
+            'previous' => '/subcategory?page=1&limit=25',
             'total' => 100,
             'limit' => 25
         ]);
